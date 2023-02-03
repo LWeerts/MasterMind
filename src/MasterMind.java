0e1b56f58f0439fa29f932153df3e72431c50a47
LWeerts <lucweerts@hotmail.com>
2023-02-03 16:00:18 +0100

    Completed part 2
    The program can now check the first letter of a hardcoded answer to the user input.
    It will print a message if it is right or wrong, and will still quit if q is entered.

--- src/MasterMind.java
+++ src/MasterMind.java
@@ -9,18 +9,22 @@ public class MasterMind {
         System.out.println("Start game");
         char quit = 'q';
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter input:");
-        String inputString = sc.nextLine();
-        System.out.println("Your input was: " + inputString);
+        String code = "abcd";
         
         for (int i = 0; i < 100; i++) {
             System.out.println("Enter input:");
-            inputString = sc.nextLine();
+            String inputString = sc.nextLine();
             if (inputString.charAt(0) == quit) {
                 System.out.println("You entered q, quitting!");
                 System.exit(0);
             } else {
                 System.out.println("Your input was: " + inputString);
+                if (inputString.charAt(0) == code.charAt(0)) {
+                    System.out.println("You guessed the right first letter");
+                    System.exit(0);
+                } else {
+                    System.out.println("You guessed the wrong first letter");
+                }
             }
         }
         
