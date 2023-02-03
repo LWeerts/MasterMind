e7bc626e90a93f9e10629e0a86c1df88e47a2988
LWeerts <lucweerts@hotmail.com>
2023-02-03 17:15:19 +0100

    Completed part 3
    The program can generate a random 4 letter code.
    If the user guesses the first letter, the whole code is displayed.

--- src/MasterMind.java
+++ src/MasterMind.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -5,11 +6,22 @@ import java.util.Scanner;
  */
 public class MasterMind {
 
+    static String generate_code() {
+        Random rand = new Random();
+        String code = "";
+        String all_letters = "abcdef";
+        for (int i = 0; i < 4; i++) {
+            int random_int = rand.nextInt(6);
+            code = code + all_letters.charAt(random_int);
+        }
+        return code;
+    }
+
     public static void main(String[] args) {
         System.out.println("Start game");
         char quit = 'q';
         Scanner sc = new Scanner(System.in);
-        String code = "abcd";
+        String code = generate_code();
         
         for (int i = 0; i < 100; i++) {
             System.out.println("Enter input:");
@@ -21,6 +33,7 @@ public class MasterMind {
                 System.out.println("Your input was: " + inputString);
                 if (inputString.charAt(0) == code.charAt(0)) {
                     System.out.println("You guessed the right first letter");
+                    System.out.println("The code is: " + code);
                     System.exit(0);
                 } else {
                     System.out.println("You guessed the wrong first letter");
